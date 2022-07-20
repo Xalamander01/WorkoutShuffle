c95ecdb7bf74ae7219eb99e3cc184635110e0811
Xalamander01 <aleksandarstanchev@abv.bg>
2022-07-20 23:15:51 +0300

    now able to read a json "dictionary" and put it in an ExerciseDictionary variable

--- src/main/java/WorkoutShuffle.java
+++ src/main/java/WorkoutShuffle.java
@@ -1,11 +1,12 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class WorkoutShuffle {
     public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
 
         Exercise overheadPress = new Exercise();
         overheadPress.setName("Overhead Press");
@@ -14,26 +15,27 @@ public class WorkoutShuffle {
         overheadPress.setCompound(true);
         //overheadPress.getAllCharacteristics();
 
+        ObjectMapper mapper = new ObjectMapper();
         Exercise benchPress = mapper.readValue(new File("src/main/resources/TestExercise.json"), Exercise.class);
         //benchPress.getAllCharacteristics();
 
-        /*
         ExerciseDictionary exerciseDictionary = new ExerciseDictionary();
-        exerciseDictionary.addExercise("overheadPress", overheadPress);
-        exerciseDictionary.addExercise("benchPress", benchPress);
+        JSONObject jsonObject = JSONUtil.parseJSONFile("src/main/resources/ExerciseDictionary.json");
+        for( int i=0; i<jsonObject.getJSONArray("exerciseDictionary").length(); i++) {
+            String exerciseShortName = (String) jsonObject.getJSONArray("exerciseDictionary").getJSONObject(i).get("ShortName");
+
+            org.json.JSONObject exercise = (org.json.JSONObject) jsonObject.getJSONArray("exerciseDictionary").getJSONObject(i).get("Exercise");
+            Exercise exerciseToAdd = new Exercise();
+            exerciseToAdd.setName((String) exercise.get("name"));
+            exerciseToAdd.setMovementType((String) exercise.get("movementType"));
+            exerciseToAdd.setLightOrHeavy((String) exercise.get("lightOrHeavy"));
+            exerciseToAdd.setCompound((boolean) exercise.get("compound"));
+            exerciseDictionary.addExercise(exerciseShortName,exerciseToAdd);
+        }
+        System.out.println(exerciseDictionary);
 
         for ( String name : exerciseDictionary.getExerciseDictionary().keySet() ) {
             exerciseDictionary.getExercise(name).getAllCharacteristics();
         }
-*/
-/*
-        ExerciseDictionary exerciseDictionary = mapper.readValue(new File("src/main/resources/ExerciseDictionary.json"), ExerciseDictionary.class);
-        exerciseDictionary.getExerciseDictionary();
-
-        for ( String name : exerciseDictionary.getExerciseDictionary().keySet() ) {
-            exerciseDictionary.getExercise(name).getAllCharacteristics();
-        }
-
- */
     }
 }
